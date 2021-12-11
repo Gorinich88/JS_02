cb7c94cea7e318853586b1ac912e32d9edaa2311
Gorinich88 <hwarang756@gmail.com>
2021-12-11 14:06:20 +0200

    add math floor

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 var cash = prompt('сколько у Вас в кошельке денег? ');
 choco = 24;
 // alert = ("Целое число " Math.floor(cash/choco);
-const nam = cash / choco;
+const nam = Math.floor(cash / choco);
 const buy = cash % choco;
-alert('Получаем целое число ' + (`${nam}`) + 'и остаток ' + (`${buy}`));
+alert('Получаем целое число ' + `${nam}` + 'и остаток ' + `${buy}`);
 
 
 // Записываем цифры задом на оборот
 var num = prompt('напишите любые три цифры ');
-const change = (('' + num).split('').reverse().join(''));
+const change = ('' + num).split('').reverse().join('');
 alert(`${change}`)
